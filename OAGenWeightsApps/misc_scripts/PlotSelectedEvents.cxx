b9335e70b8f0b487699538af7190e2d11e20200d
Dom Langridge <dominic.langridge.2023@live.rhul.ac.uk>
2026-05-11 08:31:19 -0700

    Added script for plotting selected events from the output file of makeND280SystSplines

---
+++ OAGenWeightsApps/misc_scripts/PlotSelectedEvents.cxx
@@ -0,0 +1,89 @@
+#include <cstdlib>
+#include <stdlib.h>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "TFile.h"
+#include "TObjString.h"
+#include "TTree.h"
+#include "TMacro.h"
+
+// DL: This script is for checking the selected events in OAGW spline files (i.e. outputs from makeND280SystSplines)
+//     Most of this can be done simply with TTree::Draw(), but some selections may require applying cuts based on
+//     later events, which I don't think can be done with this method? So I'm doing it here
+
+void PlotSelectedEvents() {
+
+  // Open (hardcoded) input file
+  std::string inputFileName = "/home/dlangrid/scratch/Splines/UpgradeTests/HL5.9_SplineTest_makeND280SystSplinesOutput.root";
+  TFile *inputFile = TFile::Open(inputFileName.c_str());
+
+  // Open (hardcoded) output file
+  TFile *outputFile = new TFile("SelectedEventPlots.root", "recreate");
+
+  // Get tree & branches for sample_sum
+  TTree *sample_sum = (TTree *)inputFile->Get("sample_sum");
+
+  Double_t CosThetamu;
+  Int_t SelectedSample;
+  Char_t isConsecutiveIdenticalEvent;
+
+  sample_sum->SetBranchAddress("CosThetamu", &CosThetamu);
+  sample_sum->SetBranchAddress("SelectedSample", &SelectedSample);
+  sample_sum->SetBranchAddress("isConsecutiveIdenticalEvent", &isConsecutiveIdenticalEvent);
+
+  // Get tree & branches for flattree
+  TTree *flattree = (TTree *)inputFile->Get("flattree");
+
+  Int_t Bunch;
+
+  flattree->SetBranchAddress("Bunch", &Bunch);
+
+  // Make them friends :) [might not be necessary but I think it's cute]
+  sample_sum->AddFriend("flattree");
+
+  // Create Histograms
+  TH1D *hist_All = new TH1D("hist_All", "AllSamples;", 100, -1, 1);
+  TH1D *hist_TPCmu = new TH1D("hist_TPCmu", "TPCmu;CosThetamu;", 100, -1, 1);
+  TH1D *hist_HATmu = new TH1D("hist_HATmu", "HATmu;CosThetamu;", 100, -1, 1);
+  TH1D *hist_SFGmu = new TH1D("hist_SFGmu", "SFGmu;CosThetamu;", 100, -1, 1);
+
+  // Loop over entries
+  for (uint i=0; i<sample_sum->GetEntries(); i++) {
+
+    // // Skip entries where the following entry isn't flagged as 'isConsecutiveIdenticalEvent' (based on Tomochika's investigations)
+    // if ( i != (sample_sum->GetEntries()-1) ) {
+    //   sample_sum->GetEntry(i+1);
+    //   if ( isConsecutiveIdenticalEvent != 0 ) continue; // Doesn't this always keep the last entry in an event? Is that something we want to do?
+    // }
+
+    sample_sum->GetEntry(i);
+
+    // // Skip OB (out-of-bunch) entries
+    // flattree->GetEntry(i);
+    // if ( Bunch < 0 ) continue;
+
+    // fill histogram by sample
+    hist_All->Fill(CosThetamu);
+    if ( SelectedSample == 168 ) hist_TPCmu->Fill(CosThetamu);
+    if ( SelectedSample == 169 ) hist_HATmu->Fill(CosThetamu);
+    if ( SelectedSample == 170 ) hist_SFGmu->Fill(CosThetamu);
+
+  }
+
+  // Write histograms
+  // hist_All->Write();
+  // hist_TPCmu->Write();
+  // hist_HATmu->Write();
+  // hist_SFGmu->Write();
+
+  outputFile->Write();
+}
+
+int main() {
+  PlotSelectedEvents();
+  return 0;
+}
