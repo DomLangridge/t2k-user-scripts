47a8dbfb09661443c7a131f3ed6244852f63c0ec
Dom Langridge <dominic.langridge.2023@live.rhul.ac.uk>
2026-06-03 09:33:38 -0700

    Fixed if statement

--- HighLAND2/slurm_scripts/RunCreateFlatTree_multi_job.sh
+++ HighLAND2/slurm_scripts/RunCreateFlatTree_multi_job.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=1:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --output=logs/%x/%x_%a.out
-#SBATCH --array=0
+#SBATCH --array=32,48,18,35,49,2,33
 #SBATCH --mail-user=dominic.langridge.2023@live.rhul.ac.uk
 #SBATCH --mail-type=END
 
@@ -48,7 +48,7 @@ if [ ! -f "$FLATTREE_DIR" ]; then
     exit 1
   fi
 
-  if [ ${FLATTREE_FILE##*.} -ne "root" ]; then
+  if [ ${FLATTREE_FILE##*.} != "root" ]; then
     echo "ERROR: File is not a root file"
     echo "       Exiting script"
     exit 1
