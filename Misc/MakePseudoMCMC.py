57843fda6ef0e71d6f93c72754e9941b9727e389
Dominic Langridge <dominic.langridge.2023@live.rhul.ac.uk>
2026-09-18 10:26:20 -0400

    Renamed macro and renamed step' parameter

--- Misc/MakePseudoMCMC.py
+++ Misc/MakePseudoMCMC.py
@@ -20,24 +20,24 @@ if __name__ == "__main__":
   LH_width = 1
 
 
-  # `step' here is actually the param value
-  step = [np.random.uniform(par_min,par_max)]
+  # `param' here is actually the param value
+  param = [np.random.uniform(par_min,par_max)]
   
   for i in range(n_steps):
 
-    proposed = np.random.normal(step[i], np.sqrt(step_size*(par_max-par_min)))
+    proposed = np.random.normal(param[i], np.sqrt(step_size*(par_max-par_min)))
 
-    accProb = np.minimum(1, GetLH(proposed, LH_center, LH_width)/GetLH(step[i], LH_center, LH_width) )
+    accProb = np.minimum(1, GetLH(proposed, LH_center, LH_width)/GetLH(param[i], LH_center, LH_width) )
 
     if np.random.uniform(0,1) < accProb:
-      step.append(proposed)
+      param.append(proposed)
     else:
-      step.append(step[i])
+      param.append(param[i])
 
 canvas = ROOT.TCanvas()
 
-trace = ROOT.TGraph(n_steps+1, np.linspace(0,n_steps,n_steps+1), np.array(step))
-trace.Draw()
+tracePlot = ROOT.TGraph(n_steps+1, np.linspace(0,n_steps,n_steps+1), np.array(param))
+tracePlot.Draw()
 
 canvas.Print('DummyTrace.pdf')
 canvas.Print('DummyTrace.C')
