6f364d8c81802d0391776379dc0a94133ff99fbf
Dominic Langridge <dominic.langridge.2023@live.rhul.ac.uk>
2026-09-24 08:17:18 -0400

    Allow random start point & LH, update plots, and probably more stuff

--- Misc/MakePseudoMCMC.py
+++ Misc/MakePseudoMCMC.py
@@ -2,6 +2,12 @@ import numpy as np
 import math
 import ROOT
 
+# To Do
+# - Add more parameters (increase to 10 maybe?)
+# - Start parameters outside of good LLH range (and use low n_steps, ~200) to get good trace plots
+#   - Compare to plots in Kirsty's thesis (they're very good examples)
+# - Add a way to handle crossing parameter boundaries maybe?
+
 # Combination of 1D Gaussian Likelihoods
 def GetLH(x, mean, stdev):
 
@@ -24,47 +30,69 @@ if __name__ == "__main__":
   # ===== Settings =====
 
   # General
-  saveCanvasAsC = False
-  pdf_nbins = [50, 30, 25] 
+  saveCanvasAsC = True
+  pdf_nbins = [50, 50, 50] 
   plot2DPDFs = False
+  nAccRateBatches = 20
+
+  RandomStart = False
+  RandomLH = False
+  RandomLH_precision = [3, 5, 7]
 
   # Param settings
-    # [name, min, max]
-    # Currently min & max are not actual boundaries, just a range for starting value choice
+    # [name, start, min, max]
+    # Currently min & max are not actual boundaries, just a range for random start or likelihood
   parameter = [
-    ["x", -5, 5],
-    ["y", -3, 3],
-    ["z", 0, 5]
+    ["x", 0, -99, 99],
+    ["y", 5, -99, 99],
+    ["z", -5, -99, 99]
   ]
 
-  # LH settings (currently just three arbitrary 1D Guassians)
+  # LH settings (currently just three arbitrary 1D Gaussians)
+    # Will be overwritten if RandomLH is set
   LH_mean = [
     0,
-    -1.5,
-    3
+    0,
+    0
   ]
   LH_stdev = [
     1,
-    2,
-    0.5
+    1,
+    1
   ]
 
   # Step settings
-  n_steps = 10000
+  n_steps = 100000
+  # step_size = 1.75
   step_size = 2.38/np.sqrt(len(parameter))
     # From chat w/ Henry: Ideal step size is to scale the covariance matrix variance by (2.38^2)/#parameters
     #                     This is standard dev not variance, so 2.38/sqrt(#parameters)
 
   # ====================
 
-  # Random starting step
+  # Start at 0
   par_val = [[0]]*len(parameter)
-  for p in range(len(parameter)):
-    par_val[p] = [np.random.uniform(parameter[p][1],parameter[p][2])]
+
+  # Set start position
+  if bool(RandomStart):
+    for p in range(len(parameter)):
+      par_val[p] = [np.random.uniform(parameter[p][2],parameter[p][3])]
+  else:
+    for p in range(len(parameter)):
+      par_val[p] = [parameter[p][1]]
+
+  # Set random LH if on
+  if bool(RandomLH):
+    for p in range(len(parameter)):
+      LH_mean[p] = np.random.uniform(parameter[p][2],parameter[p][3])
+      LH_stdev[p] = abs(np.random.normal(0,abs(parameter[p][3]-parameter[p][2])/RandomLH_precision[p]))
   
   print("")
   print("Starting from initial position ",par_val)
   print("Using step size ",step_size)
+  print("")
+  print("With Gaussian LH mean ", LH_mean)
+  print("              & stdev ", LH_stdev)
   print("")
 
   # Counters
@@ -108,20 +136,25 @@ if __name__ == "__main__":
   print("")
 
   canvas = ROOT.TCanvas()
+  # gStyle.SetOptStat(0)
 
   # plot trace
 
+  print("> Plotting Trace")
   canvas.Print('DummyTrace.pdf[')
 
-  print("> Plotting Trace")
   for p in range(len(parameter)):
 
     tracePlot = ROOT.TGraph(n_steps+1, np.linspace(0,n_steps,n_steps+1), np.array(par_val[p]))
 
     tracePlot.GetXaxis().SetTitle("Step")
-    tracePlot.GetYaxis().SetTitle(parameter[p][0])
+    tracePlot.SetTitle("")
+    # tracePlot.GetYaxis().SetTitle(parameter[p][0]) # For testing
+    tracePlot.GetYaxis().SetTitle("Parameter Value") # for MaCh3 paper plots
+
     tracePlot.Draw()
 
+    canvas.SetLogx()
     canvas.Print('DummyTrace.pdf')
     if bool(saveCanvasAsC):
       canvas.Print('DummyTrace_'+parameter[p][0]+'.C')
@@ -135,9 +168,15 @@ if __name__ == "__main__":
   # plot accRate
 
   print("> Plotting Acceptance Rate")
+  canvas.Print('DummyAccProb.pdf[')
+
+    # graph
   accRatePlot = ROOT.TGraph(n_steps, np.linspace(1,n_steps,n_steps), np.array(accRate))
+
+  accRatePlot.SetTitle("")
   accRatePlot.GetXaxis().SetTitle("Step")
-  accRatePlot.GetYaxis().SetTitle("Acceptance Rate")
+  accRatePlot.GetYaxis().SetTitle("Acceptance Probability")
+  accRatePlot.GetYaxis().SetRangeUser(0, 1)
   accRatePlot.Draw()
 
   canvas.Print('DummyAccProb.pdf')
@@ -146,27 +185,52 @@ if __name__ == "__main__":
 
   canvas.Clear()
 
-  # plot pdf
+    # batched hist
+  accRateBatchPlot = ROOT.TH1D("", "", nAccRateBatches, 0, n_steps)
+  for step in range(len(accRate)):
+    accRateBatchPlot.Fill(step, accRate[step]*nAccRateBatches/(n_steps+1))
 
-  canvas.Print('DummyPDF.pdf[')
+  for batch in range(nAccRateBatches):
+    batch_min = batch*(n_steps//nAccRateBatches)
+    batch_max = (batch+1)*(n_steps//nAccRateBatches)
+    accRateBatchPlot.GetXaxis().SetBinLabel(batch+1,str(batch_min)+" - "+str(batch_max))
 
-  # 1D pdf with target
+  accRateBatchPlot.SetTitle("")
+  accRateBatchPlot.SetXTitle("")
+  accRateBatchPlot.SetYTitle("Acceptance Probability")
+  accRateBatchPlot.GetYaxis().SetRangeUser(0,1)
+  accRateBatchPlot.Draw("HIST")
+  
+  canvas.Print('DummyAccProb.pdf')
+  if bool(saveCanvasAsC):
+    canvas.Print('DummyAccProb_Batch.C')
+
+
+  canvas.Print('DummyAccProb.pdf]')
+  canvas.Clear()
+
+  # plot 1D pdf
 
   print("> Plotting 1D PDFs")
+  canvas.Print('DummyPDF.pdf[')
+
   for p in range(len(parameter)):
 
-    pdfPlot = ROOT.TH1D(parameter[p][0]+" PDF",parameter[p][0]+" PDF", pdf_nbins[p], parameter[p][1], parameter[p][2])
+    par_min = min(par_val[p])
+    par_max = max(par_val[p])
+
+    pdfPlot = ROOT.TH1D(parameter[p][0]+" PDF",parameter[p][0]+" PDF", pdf_nbins[p], par_min, par_max)
     for v in par_val[p]:
-      pdfPlot.Fill(v)
+      pdfPlot.Fill(v, (pdf_nbins[p]/(par_max-par_min)))
 
     pdfPlot.SetXTitle(parameter[p][0])
-    pdfPlot.SetYTitle("Steps/"+str(n_steps))
+    pdfPlot.SetYTitle("Steps/("+str(n_steps)+"*"+str((par_max-par_min)/pdf_nbins[p])+")")
     pdfPlot.Draw("HIST")
     pdfPlot.Scale(1/n_steps)
 
-    x = np.linspace(parameter[p][1],parameter[p][2],1000)
+    x = np.linspace(par_min,par_max,1000)
     # targetDist = ROOT.TGraph(1000, x, GetLH(x, LH_mean[p], LH_stdev[p]))
-    targetDist = ROOT.TGraph(1000, x, GetLH(x, LH_mean[p], LH_stdev[p])*((parameter[p][2]-parameter[p][1])/pdf_nbins[p]))
+    targetDist = ROOT.TGraph(1000, x, GetLH(x, LH_mean[p], LH_stdev[p]))
     targetDist.Draw("SAME")
 
     canvas.Print('DummyPDF.pdf')
@@ -175,24 +239,29 @@ if __name__ == "__main__":
 
     canvas.Clear()
 
-  # 2D pdf
+  # plot 2D pdf
 
   if bool(plot2DPDFs):
     print("> Plotting 2D PDFs (this may take a while...)")
     for p in range(len(parameter)-1):
+      par_min_x = min(par_val[p])
+      par_max_x = max(par_val[p])
+      
       for q in range(p+1,len(parameter)):
+        par_min_y = min(par_val[q])
+        par_max_y = max(par_val[q])
 
         print("  ",parameter[p][0],", ",parameter[q][0])
 
-        pdfPlot2D = ROOT.TH2D(parameter[p][0]+" "+parameter[q][0]+" PDF",parameter[p][0]+" "+parameter[q][0]+" PDF", pdf_nbins[p], parameter[p][1], parameter[p][2], pdf_nbins[q], parameter[q][1], parameter[q][2])
+        pdfPlot2D = ROOT.TH2D(parameter[p][0]+" "+parameter[q][0]+" PDF",parameter[p][0]+" "+parameter[q][0]+" PDF", pdf_nbins[p], par_min_x, par_max_x, pdf_nbins[q], par_min_y, par_max_y)
 
         for v1 in par_val[p]:
           for v2 in par_val[q]:
-            pdfPlot2D.Fill(v1, v2)
+            pdfPlot2D.Fill(v1, v2, (pdf_nbins[p]/(par_max_x-par_min_x))*(pdf_nbins[q]/(par_max_y-par_min_y)))
 
         pdfPlot2D.SetXTitle(parameter[p][0])
         pdfPlot2D.SetYTitle(parameter[q][0])
-        pdfPlot2D.SetZTitle("Steps/"+str(n_steps))
+        pdfPlot2D.SetZTitle("Steps/("+str(n_steps)+"*"+str((par_max_x-par_min_x)/pdf_nbins[p])+"*"+str((par_max_y-par_min_y)/pdf_nbins[q])+")")
         pdfPlot2D.Draw("HIST COLZ")
         pdfPlot2D.Scale(1/n_steps)
 
